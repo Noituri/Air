5649f6a1ca6053a4edf21d1a0113ee33cd89aef2
Mikolaj Radkowski <yknomeh@protonmail.com>
2019-03-19 10:25:54 +0100

    some layout changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,23 +12,38 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  final Shader linearGradient = LinearGradient(colors: <Color>[Color(0xffDA44bb), Color(0xff8921aa)],).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        child: Stack(
+          fit:StackFit.expand,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container (
+              margin: EdgeInsets.only(top: 120),
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: <Widget>[
+                  Text (
+                    "Here's gonna be icon \n also circle must be bigger"
+                  ),
+                  Text(
+                "0 / (light Color) 100",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  foreground: Paint()..shader = linearGradient,
+                ),
+                ),
+                ],
+              ),
             ),
             FlareActor(
               "assets/progress.flr",
-              alignment:Alignment.center,
-              fit:BoxFit.contain,
+              alignment: Alignment.topCenter,
               animation: "0to100"
             ),
           ],
